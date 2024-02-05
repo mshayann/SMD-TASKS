e27b2d44f854c53b029da41b765259302f3476be
Shayan <shayan.saleem24@gmail.com>
2024-02-05 20:10:34 +0500

    Lab 2 task

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<String> todo_tasks = [
+    "Take notes of DDR",
+    "Meeting at 2",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: Column(
+        children: [
+          Column(
+            children: todo_tasks.map((e) => Text(e)).toList(),
+          ),
+          TextField(),
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Add'),
+          ),
+        ],
+      ),
+    );
+  }
+}
